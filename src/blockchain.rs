5111196588c2d33f2687636c97fecbe8f3b16d52
Ahmet Kaan GÜMÜŞ <96421894+Tahinli@users.noreply.github.com>
2024-05-22 04:23:43 +0300

    feat: :sparkles: data and hash time cost added to the chain

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 use chrono::Utc;
 
 use crate::block::Block;
@@ -14,9 +16,11 @@ impl BlockChain {
         let genesis_block = Block {
             index: 0,
             timestamp: Utc::now().timestamp_millis() as u64,
+            data: String::from("Tahinli"),
             proof_of_work: 0_u64,
             previous_hash: String::new(),
             hash: String::new(),
+            hash_time_cost: Duration::from_secs(0),
         };
 
         let chain = vec![genesis_block.clone()];
@@ -28,10 +32,12 @@ impl BlockChain {
         }
     }
 
-    pub fn add_block(&mut self) {
+    pub fn add_block(&mut self, data: String, instant: Instant) {
         let new_block = Block::new(
             self.chain.len() as u64,
+            data,
             self.chain[&self.chain.len() - 1].hash.clone(),
+            instant,
         )
         .mine(self.clone());
         self.chain.push(new_block);
